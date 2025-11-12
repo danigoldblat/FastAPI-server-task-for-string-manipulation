c0994fa9533ceb88b84738179c3e3a1eb1da9e52
danigoldblat <d0504109212@gmail.com>
2025-11-12 17:16:05 +0200

    remove_vowels

--- string_ops.py
+++ string_ops.py
@@ -1,7 +1,7 @@
-# def reverse_str(s: str) -> str:
-#     return s[::-1]
-# def to_upper(s: str) -> str:
-#     return s.upper
+def reverse_str(s: str) -> str:
+    return s[::-1]
+def to_upper(s: str) -> str:
+    return s.upper
 def remove_vowels(s: str) -> str:
     vowels=["a","e","i","o","u"]
     for i in s:
